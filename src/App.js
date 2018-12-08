fc1d862f6d262a68a96323fb357da8efd1886f2a
unknown <h2hallowdy@gmail.com>
2018-12-08 23:07:26 +0700

    Apply Card component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import StarRating from './components/StarRating';
+import Card from './components/Card';
+
 import './App.css';
 
 class App extends Component {
@@ -8,6 +10,11 @@ class App extends Component {
     return (
       <div className="App">
         <StarRating number="4.5"/>
+
+        <Card imageUrl="https://picsum.photos/200/300">
+          <h3>Card-title</h3>
+          <p>Card content</p>
+        </Card>
       </div>
     );
   }
