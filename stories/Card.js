9865676b166775ef500ba03617ebf455e90c16b8
unknown <h2hallowdy@gmail.com>
2018-12-08 23:14:55 +0700

    Make some change to use show info in storybook

--- stories/Card.js
+++ stories/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 // import { withInfo } from '@storybook/addon-info';
 import Card from '../src/components/Card';
 
