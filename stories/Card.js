23007512c854df602d5e184c2c351cc8486cbc8e
unknown <h2hallowdy@gmail.com>
2018-12-08 23:08:09 +0700

    Add new story Card to storybook

---
+++ stories/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+// import { withInfo } from '@storybook/addon-info';
+import Card from '../src/components/Card';
+
+
+storiesOf('Card', module)
+    .add(
+        'Simple-Version',
+        () => (
+            <Card imageUrl="https://picsum.photos/200/300">
+                <h3>Card-title</h3>
+                <p>Card content</p>
+            </Card>
+        )
+    );
