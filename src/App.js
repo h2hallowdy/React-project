7dc425b24148f4cae41d062efc73cbfbdd38e7cd
unknown <h2hallowdy@gmail.com>
2018-12-09 18:01:57 +0700

    Update all component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-
-import StarRating from './components/StarRating';
-import Card from './components/Card';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import StarRating from './components/StarRating';
+// import Card from './components/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import TopMenu from './components/TopMenu';
+import Products from './pages/Products';
+const Index = () => <h2>Home</h2>;
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <StarRating number="4.5"/>
+      <Router>
+        <div className="App">
+          <TopMenu />
+          
+          <Route path="/" exact component={Index}></Route>
+          <Route path="/products/" exact component={Products}></Route>
 
-        <Card imageUrl="https://picsum.photos/200/300">
-          <h3>Card-title</h3>
-          <p>Card content</p>
-        </Card>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
